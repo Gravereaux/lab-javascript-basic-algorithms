6d57523fcd026f9342da12adf134fff51a1c8c16
Natalia Gravereaux <natalia.gravereaux@ose-engineering.fr>
2022-08-03 23:05:10 +0200

    LAB - JavaScript Basic Algorithms Natalia Gravereaux

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 // Iteration 1: Names and Input
-//
+let hacker1 = "XXXX";
+
+console.log(`The driver's name is ${hacker1}`);
+
+let hacker2 = "YYYY";
+
+console.log(`The navigator's's name is ${hacker2}`);
+
 // Iteration 2: Conditionals
+const str = "navigator";
+console.log(`It seems that the navigator has the longest name, it has ${str.length} characters`);
 
 
 // Iteration 3: Loops
